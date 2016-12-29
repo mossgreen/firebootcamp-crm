bbe7939ecb74da478e9fb5b8d07cb261eb403a40
Feifei GU <gufeifei@outlook.com>
2016-12-30 12:22:12 +1300

    Add the company and contact model files

---
+++ src/app.config.ts
@@ -0,0 +1,1 @@
+export const API_BASE = 'http://firebootcamp-crm-api.azurewebsites.net/api/';
---
+++ src/app/contact/contact.ts
@@ -0,0 +1,12 @@
+export class Contact {
+    id: number;
+    companyId: number;    
+    name: string;
+    address: string;
+    city: string;
+    state: string;
+    postCode: string;
+    phone: string;
+    email: string;
+    companyName: string;
+}
