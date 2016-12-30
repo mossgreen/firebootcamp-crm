7ed91d6eaf5d4419e0bfe2eefcf2aacdde97aed8
Feifei GU <gufeifei@outlook.com>
2016-12-30 13:26:20 +1300

    company-list.component.ts

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,11 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from './../../shared/models';
+import { CompanyService } from './../company.service';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 
 @Component({
     template: `
-        <div class="row">
-           <h2>Companies</h2>
+        <div class="row heading-row company-list-component">
+            <div class="col-sm-9>
+                <h2>Companies</h2>
+            </div>
+
+            <div class="col-sm-3">
+                <button routerLink="/company/edit/new" class="btn btn-success pull-right">Add</button>
+            </div>
+       </div>
+
+       <div class="row">
+            <div class="col-sm-12>
+                <fbc-company-list-table
+                    (deleteCompanySelected)="deleteCompany($event)"
+                    [companies]="companies">
+                </fbc-company-list-table>
+            </div>
        </div>
     `,
 })
-export class CompanyListComponent {    
+export class CompanyListComponent implements OnInit {
+    companies: Company[];
+    result: any;
+
+    constructor(private _companyService: CompanyService) { }
+
+    ngOnInit() {
+        this.getCompanies();
+    }
+    getCompanies() {
+        this._companyService.getCompanies()
+            .subscribe((companies: Company[]) => this.companies = companies);
+    }
+
+    deleteCompany(companyId: number) {
+        this._companyService.deleteCompany(companyId)
+            .subscribe((deletedCompay: Company) => {
+                this.companies = this.companies.filter((company: any) => company.id !== deletedCompay.id);
+            });
+    }
 }
