e809aed3b23a71649589218d292cdff53bca5d32
Feifei GU <gufeifei@outlook.com>
2016-12-31 15:26:36 +1300

    change router to singular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { ModuleWithProviders } from '@angular/core';
 
 const appRoutes: Routes = [
     { path: '', loadChildren: './home/home.module#HomeModule' },
-    { path: 'companies', loadChildren: './company/company.module#CompanyModule' },
-    { path: 'contacts', loadChildren: './contact/contact.module#ContactModule' },
+    { path: 'company', loadChildren: './company/company.module#CompanyModule' },
+    { path: 'contact', loadChildren: './contact/contact.module#ContactModule' },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
