f01b537b53df09adc2da72f32c96b84276215c47
Feifei GU <gufeifei@outlook.com>
2017-01-01 22:45:09 +1300

    Update contact-list.component.ts

--- src/app/contact/contact-list/contact-list.component.ts
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './../../shared/models';
+import { ContactService } from './../contact.service';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+
 
 @Component({
     template: `
-     <div class="row">
-        <div class="col-sm-3">
+     <div class="row heading-row contact-list-component">
+        <div class="col-sm-9">
            <h2>Contacts</h2>
+        </div>
+        <div class="col-sm-3"
+            <button routerLink="/contact/edit/new" class="btn btn-success pull-right">Add</button>
+        </div>
+     </div>
+
+     <div class="row>
+        <div class="col-sm-12">
+            <fbc-contact-list-table (deleteContactSelected)="deleteContact($event)" [contacts]="contacts">
+            </fbc-contact-list-table>
         </div>
      </div>
     `,
 })
-export class ContactListComponent {    
+export class ContactListComponent implements OnInit {
+    contacts: Contact[];
+    result: any;
 
+    constructor(private _contactService) { }
+
+    ngOnInit() { this.getContacts(); }
+
+    getContacts() {
+        this._contactService.getContacts()
+            .subscribe((contacts: Contact[]) => this.contacts = contacts);
+    }
+
+    deleteContact(contactId: number) {
+        this._contactService.deleteContact(contactId)
+            .subscribe((deletedContact: Contact) => {
+                this.contacts = this.contacts.filter((contact: any) =>
+                    contact.id !== deletedContact.id);
+            });
+    }
 }
