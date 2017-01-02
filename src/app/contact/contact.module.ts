ab2aef8b7cea4b84212e205d573e4e0b571e4b1a
Feifei GU <gufeifei@outlook.com>
2017-01-02 19:50:20 +1300

    Add ContactEditFormComponent

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,5 +1,6 @@
 import { ContactListComponent } from './contact-list/contact-list.component';
-
+import {ContactEditFormComponent} from './contact-edit-form.component';
+import {ContactDetailComponent} from './contact-detail-component';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { routing } from './contact.routes';
@@ -8,6 +9,8 @@ import { routing } from './contact.routes';
     imports: [SharedModule, routing],
     declarations: [
         ContactListComponent,
+        ContactDetailComponent,
+        ContactEditFormComponent
         ]
 })
 export class ContactModule { }
