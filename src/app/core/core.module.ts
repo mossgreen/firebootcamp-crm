26d69c205cf70423e8fbb3b4f5e386cd8c6e0b48
Feifei GU <gufeifei@outlook.com>
2017-01-26 23:35:01 +1300

    Add comments to CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import {CompanyService} from '../company/company.service';
 import {ContactService} from '../contact/contact.service';
 
-
+/**
+ * Each time the Shared module is instantiated within a lazy-loaded component a new instance of it's services is created. 
+ * Especially when dealing with services that maintain state this is a probem.
+ * Rather than declaring all of these services in the AppModule (will only instantiated once)
+ * best to create a core module, declare the globally available service in the core module, and import the core module into the app module
+ * 
+ * the sole purpose id to declare application wide singleton services
+ */
 @NgModule({
     imports: [],
     exports: [],
