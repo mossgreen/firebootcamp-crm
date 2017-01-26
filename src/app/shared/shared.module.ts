ce696c5a58831ad23d40977cb136f442990a676b
Feifei GU <gufeifei@outlook.com>
2017-01-26 23:28:45 +1300

    Adding comments to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,14 +5,24 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import {UIRowComponent} from './ui-row.component';
 
+/**
+ * SharedModule holds common components, directives and pips and allows them to be shared with any modules that need them.
+ * Most components in the system will need the FormsModule and the HttpModule,
+ * so it makes sense to include them once on the shared module.
+ */
 @NgModule({
     imports: [
-        CommonModule,
+        CommonModule,//SharedModule needs common directives
         RouterModule,
         HttpModule,
         FormsModule,
         ReactiveFormsModule,        
     ],
+    /**
+     * NOTE: we import them and re-export them
+     * doing so we enable all of those modules to not need to include those imports themselves
+     */
+
     exports: [
         CommonModule,
         RouterModule,
