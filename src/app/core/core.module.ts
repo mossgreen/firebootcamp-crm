d3dd936e259f557ca18eb5f0b7508f63e56dc4c1
Feifei GU <gufeifei@outlook.com>
2017-04-16 18:46:42 +1200

    Add Core and Shared Module to App module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core'
+
+@NgModule({
+    imports:[],
+    exports:[],
+    declarations:[],
+    providers:[
+        //services that we want to declare globally will go here
+    ],
+})
+
+export class CoreModule{}
