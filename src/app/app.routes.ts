11f480f1cd9973edf1a47f9b32045e3cfd726ce0
Feifei GU <gufeifei@outlook.com>
2017-04-16 19:34:27 +1200

    add router to app

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+const appRoutes: Routes = [
+    { path: '', loadChildren: 'app/home/home.module#HomeModule' },
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    template:
+    `
+        <h1>Welcome to the Moss CRM App</h1>
+    `
+})
+
+export class HomeComponent{}
